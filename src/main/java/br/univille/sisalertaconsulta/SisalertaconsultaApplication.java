b95c38b5db76d047c3f903283c601231f678f413
PriFernandaA <priscilafanacleto@gmail.com>
2022-11-10 14:20:00 -0300

    first commit

---
+++ src/main/java/br/univille/sisalertaconsulta/SisalertaconsultaApplication.java
@@ -0,0 +1,13 @@
+package br.univille.sisalertaconsulta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisalertaconsultaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisalertaconsultaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/univille/sisalertaconsulta/SisalertaconsultaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.sisalertaconsulta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisalertaconsultaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
