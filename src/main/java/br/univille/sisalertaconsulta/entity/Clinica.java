779d775adc631866778f6f0260c3b006458ecece
PriFernandaA <priscilafanacleto@gmail.com>
2022-11-20 20:59:08 -0300

    Implementação entdade clinica

---
+++ src/main/java/br/univille/sisalertaconsulta/entity/Clinica.java
@@ -0,0 +1,45 @@
+package br.univille.sisalertaconsulta.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Clinica {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(length = 100)
+    private String nome;
+    @Column(length = 1000)
+    private String localizacao;
+    @Column(length =11)
+    private String telefone;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getLocalizacao() {
+        return localizacao;
+    }
+    public void setLocalizacao(String localizacao) {
+        this.localizacao = localizacao;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
