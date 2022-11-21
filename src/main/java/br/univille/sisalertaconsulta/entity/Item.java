b753d8e74f09ce65732f54e723ff105efdfe8d15
PriFernandaA <priscilafanacleto@gmail.com>
2022-11-20 22:18:26 -0300

    Implementação entidade Item

---
+++ src/main/java/br/univille/sisalertaconsulta/entity/Item.java
@@ -0,0 +1,29 @@
+package br.univille.sisalertaconsulta.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(length = 100)
+    private String nome;
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
